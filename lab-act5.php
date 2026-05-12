641641ced53b82e36558dcdee7151e6f9f6f229f
Ysabelleaaa <jmgadot@paterostechnologicalcollege.edu.ph>
2026-05-12 20:40:59 +0800

    Add simple form to secure user input

---
+++ lab-act5.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="">
+ Name: <input type="text" name="name">
+ <input type="submit">
+</form>
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+if(empty($_POST["name"])) {
+ echo "Name is required";
+} else {
+ $name = htmlspecialchars($_POST['name']);
+ echo "Hello, " . $name;
+}
+
+}
+?>
+
+</body>
+</html>
