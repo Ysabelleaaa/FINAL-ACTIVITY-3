4a7ee748160b8a789697733645881fb8f05351c4
Ysabelleaaa <jmgadot@paterostechnologicalcollege.edu.ph>
2026-05-12 20:11:18 +0800

    form.php

---
+++ lab-act19 (FORM).php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="process.php">
+ Name: <input type="text" name="name"><br><br>
+ Email: <input type="text" name="email">
+ <br><br>
+ <input type="submit">
+</form>
+
+</body>
+</html>
